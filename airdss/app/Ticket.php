c84d3f7785414e1b8bde315d7544df7ba664f005
Alejandro Panagiotidis <alejandro@MacBook-Pro-de-Alejandro-2.local>
2019-02-26 18:52:25 +0100

    migración Ticket

--- airdss/app/Ticket.php
+++ airdss/app/Ticket.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    public function client() {
+        // Product tiene la clave ajena 'category_id'
+        return $this->belongsTo('App\client'); 
+    }
+
+    public function boardingpass() {
+        return $this->hasMany('App\boardingpass'); 
+    }
+
+    public function flight() {
+        return $this->belongsTo('App\flight'); 
+    }
 }
--- airdss/database/migrations/2019_02_26_163433_create_tickets_table.php
+++ airdss/database/migrations/2019_02_26_163433_create_tickets_table.php
@@ -15,6 +15,11 @@ class CreateTicketsTable extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->increments('id');
+            $table->intger('codigo')->unique();
+            $table->string('asiento');
+            $table->string('clase');
+            $table->date('fecha');
+            $table->foreign('boardingpass_id')->references('id')->on('boardingpass');
             $table->timestamps();
         });
     }
