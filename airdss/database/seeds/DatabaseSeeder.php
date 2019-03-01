2c5e6fe8acdac0d8ba075e98efc7df0fb8d28016
Alejandro Panagiotidis <alejandro@MacBook-Pro-de-Alejandro-2.local>
2019-03-01 19:12:57 +0100

    entrega1

--- airdss/database/seeds/DatabaseSeeder.php
+++ airdss/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //Habría que mirar en que orden ejecutar los distintos seeders
+        //Mirar en que orden ejecutar los distintos seeders
+    
         $this->call(ClientTableSeeder::class);
         $this->call(TicketsTableSeeder::class);
         $this->call(BoardingPassesTableSeeder::class);
