e9b3de230e7550b5f5775525a744daa86d0ba43c
Abraham Perez <ajpr3@alu.ua.es>
2019-03-12 19:26:51 +0100

    cambio cliente

--- airdss/app/Client.php
+++ airdss/app/Client.php
@@ -6,13 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    public $id;
-    public $dni;
-    public $nombre;
-    public $apellido1;
-    public $apellido2;
-    public $telefono;
-    public $email;
+
 
     public function tickets(){
         return $this->hasMany('App\Ticket');
