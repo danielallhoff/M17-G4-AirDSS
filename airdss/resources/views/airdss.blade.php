83fa20cc9c3e43f701a6d6152f1b4edacbf1f380
Alejandro Panagiotidis <alejandro@MacBook-Pro-de-Alejandro-2.local>
2019-05-14 17:48:05 +0200

    Cambio inicio

--- airdss/resources/views/airdss.blade.php
+++ airdss/resources/views/airdss.blade.php
@@ -4,31 +4,33 @@
     </head>
 @section('contenido')
 <div class="container-fluid">
-    <div class="intro">
-        <p style="font-size:70px;color:white">Bienvenido a AirDSS</p>
-        <p style="font-size:30px;color:white">Le damos vuelo a tus sueños.</p>
-    </div>
-    <div class="cards">
-        <div class="tarjeta">
-            <img src="../images/Puntual.jpg" alt="Avatar" style="width:100%;height:50%">
-            <div class="container">
-                <h2><b>Puntual</b></h2> 
-                <p style="font-size:20px">AirDSS se destaca en el sector por ser la compañía más puntual.</p> 
-            </div>
+    <div class="centrado">
+        <div class="intro">
+            <p style="font-size:70px;color:white">Bienvenido a AirDSS</p>
+            <p style="font-size:30px;color:white">Le damos vuelo a tus sueños.</p>
         </div>
-        <div class="tarjeta">
-            <img src="../images/Rapido.jpg" alt="Avatar" style="width:100%;height:50%">
-            <div class="container">
-                <h2><b>Sencillo</b></h2> 
-                <p style="font-size:20px">En nuestra web podrás comprar de manera rápida y sencilla tu próximo
-                    vuelo.</p> 
+        <div class="cards">
+            <div class="tarjeta">
+                <img src="../images/Puntual.jpg" alt="Avatar" style="width:100%;height:50%">
+                <div class="container">
+                    <h2><b>Puntual</b></h2> 
+                    <p style="font-size:20px">AirDSS se destaca en el sector por ser la compañía más puntual.</p> 
+                </div>
             </div>
-        </div>
-        <div class="tarjeta">
-            <img src="../images/Servicio.jpg" alt="Avatar" style="width:100%;height:50%">
-            <div class="container">
-                <h2><b>Servicio</b></h2> 
-                <p style="font-size:20px">AirDSS se caracteriza por su excelente servicio y atención al cliente</p> 
+            <div class="tarjeta">
+                <img src="../images/Rapido.jpg" alt="Avatar" style="width:100%;height:50%">
+                <div class="container">
+                    <h2><b>Sencillo</b></h2> 
+                    <p style="font-size:20px">En nuestra web podrás comprar de manera rápida y sencilla tu próximo
+                        vuelo.</p> 
+                </div>
+            </div>
+            <div class="tarjeta">
+                <img src="../images/Servicio.jpg" alt="Avatar" style="width:100%;height:50%">
+                <div class="container">
+                    <h2><b>Servicio</b></h2> 
+                    <p style="font-size:20px">AirDSS se caracteriza por su excelente servicio y atención al cliente</p> 
+                </div>
             </div>
         </div>
     </div>
