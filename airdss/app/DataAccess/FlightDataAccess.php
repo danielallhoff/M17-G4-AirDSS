6d8bd6ed74adcca6ca952da325232d9d8520ecaf
Berta Murcia <bmm81@alu.ua.es>
2019-05-15 11:29:30 +0200

    arreglo data access

--- airdss/app/DataAccess/FlightDataAccess.php
+++ airdss/app/DataAccess/FlightDataAccess.php
@@ -74,7 +74,7 @@ class FlightDataAccess
     
     static public function edit(Request $request){
         
-        $flight = new F();
+        $flight = F::findOrFail($request->id);
         $flight->capacidad = $request->input('capacidad');
         $flight->airport_origen_id = $request->input('origen');
         $flight->airport_destino_id = $request->input('destino');
